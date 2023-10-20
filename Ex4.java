f3b3e4b711fc9f521b1572a180674093ebc3f853
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-20 19:36:58 -0400

    Create Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -1,1 +1,27 @@
+/* Make a programm to read a numbre N. Then read N pairs of numbers and show the division of the first by the second. If the denominator is equal to zero, show the message "impossible division". */
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of divisions that will be calculated:");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the pairs of numbers:");
+            int a = sc.nextInt(), b = sc.nextInt();
+
+            if (b != 0) {
+                double div = (double) a / b;
+                System.out.printf("%.1f%n", div);
+            } else {
+                System.out.println("Impossible division");
+            }
+        }
+        
+        sc.close();
+    }
+}
 
